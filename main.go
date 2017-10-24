6504a9af8e252246d39a8c1769fcce1cf565c99e
Luis Ángel Méndez Gort <gort.andres000@gmail.com>
2017-10-24 15:22:37 -0400

    Trying with WithContext

--- main.go
+++ main.go
@@ -20,8 +20,23 @@ func main() {
 	log.Fatal(http.ListenAndServe(*addr, px))
 }
 
+type proxy struct {
+	px *gp.ProxyHttpServer
+}
+
+// RemoteAddr is the type to be used as key
+// of RemoteAddr value in context
+type RemoteAddr string
+
+func (p *proxy) serveHTTP(w http.ResponseWriter, r *http.Request) {
+	q := r.WithContext(context.WithValue(context.Background(),
+		RemoteAddr("RemoteAddress"), r.RemoteAddr))
+	p.serveHTTP(w, q)
+}
+
 func dialHTTP(c context.Context, nt, ad string) (n net.Conn, e error) {
-	fmt.Printf("%v\n", c)
+	v := c.Value(RemoteAddr("RemoteAddress"))
+	fmt.Printf("%v\n", v)
 	n, e = net.Dial(nt, ad)
 	return
 }
